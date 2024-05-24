761f6a8951a65458e2eada9fd0592d9a7b4d6be0
vic-3PO <victma2001@gmail.com>
2024-05-24 16:44:28 -0300

    backend

--- Bakcend/routes/api.php
+++ Bakcend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DriverController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\TripController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +12,13 @@ Route::post('/login/verify', [LoginController::class,'verify']);
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/driver', [DriverController::class, 'show']);
     Route::post('/driver', [DriverController::class, 'update']);;
+
+    Route::post('/trip',[TripController::class,'store']);
+    Route::get('/trip/{trip}', [TripController::class,'show']);
+    Route::post('/trip/{trip}/accept', [TripController::class, 'accept']);
+    Route::post('/trip/{trip}/start', [TripController::class, 'start']);
+    Route::post('/trip/{trip}/end', [TripController::class, 'end']);
+    Route::post('/trip/{trip}/location', [TripController::class, 'location']);
 
     Route::get('/user', function(Request $request){
         return $request->user();
