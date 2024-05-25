14b8272bda43c44d67ee17d2f6f4cb5fb442315e
vic-3PO <victma2001@gmail.com>
2024-05-25 15:00:47 -0300

    atualização

---
+++ Frontend/src/views/LoginView.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="pt-16">
+        <h1 class="text-3xl font-semibold mb-4">Enter your email</h1>
+        <form v-if="!waitingOnVerification" action="#" @submit.prevent="handleLogin">
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <input type="email" v-model="credentials.email" name="email" id="email" placeholder="example@example.com"
+                            class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
+                    </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button type="submit" @submit.prevent="handleLogin"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Continue</button>
+                </div>
+            </div>
+        </form>
+        <form v-else action="#" @submit.prevent="handleVerification">
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <input type="text" v-maska data-maska="######" v-model="credentials.login_code" name="login_code" id="login_code" placeholder="123456"
+                            class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
+                    </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button type="submit" @submit.prevent="handleVerification"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Verify</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+
+<script setup>
+import {vMaska} from 'maska'
+import { reactive} from 'vue'
+import axios from 'axios'
+
+const credentials = reactive({
+    email: null
+})
+
+const handleLogin = () => {
+    axios.post("http://127.0.0.1:8000/api/login/", credentials)
+        .then((response) => {
+            console.log(response.data)
+        })
+        .catch((error) => {
+            console.error(error)
+            alert(error.response.data.message)
+        })
+}
+</script>
